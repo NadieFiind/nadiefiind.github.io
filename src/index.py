6dd4ff1cb6bde032fa45e4e47c84254bad958039
NadieFiind <nadiefiind@gmail.com>
2022-11-13 15:46:30 +0800

    Accept argument in the page builder

--- src/index.py
+++ src/index.py
@@ -1,36 +1,39 @@
-from typing import Callable
+from typing import Any, Callable
 from pyfyre import render, Style
 from pyfyre.nodes import *
 from pages import Home, About, Skills, Projects
 from components import Background, Nav, SurpriseMessage
 
 
-def page_builder(main: Element) -> Callable[[], Element]:
-    return lambda: Element(
-        "div",
-        lambda: [
-            Background(),
-            main,
-            Nav(),
-            SurpriseMessage(),
-            Link(
-                "https://twitter.com/adityar51253736/status/1584827745112317953"
-                "?t=0518MJBJHHmQZxub23fPhA&s=19",
-                lambda: [Text("Background Source")],
-                styles=[
-                    Style(
-                        position="fixed",
-                        bottom="0",
-                        font_family="Syne Mono",
-                        opacity="0.5",
-                        font_size="1rem",
-                        z_index="2000",
-                    )
-                ],
-                attrs={"target": "_blank"},
-            ),
-        ],
-    )
+def page_builder(main: Element) -> Callable[[Any], Element]:
+    def route_builder(arg: Any) -> Element:
+        return Element(
+            "div",
+            lambda: [
+                Background(),
+                main,
+                Nav(),
+                SurpriseMessage(),
+                Link(
+                    "https://twitter.com/adityar51253736/status/1584827745112317953"
+                    "?t=0518MJBJHHmQZxub23fPhA&s=19",
+                    lambda: [Text("Background Source")],
+                    styles=[
+                        Style(
+                            position="fixed",
+                            bottom="0",
+                            font_family="Syne Mono",
+                            opacity="0.5",
+                            font_size="1rem",
+                            z_index="2000",
+                        )
+                    ],
+                    attrs={"target": "_blank"},
+                ),
+            ],
+        )
+
+    return route_builder
 
 
 render(
